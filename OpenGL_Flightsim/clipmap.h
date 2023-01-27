21d1957ebff00f40c6e6b5b1b23f66646771809d
Jakob <jakob.g.maier@gmail.com>
2023-01-27 09:40:38 +0100

    implemented ring fix-up

--- OpenGL_Flightsim/clipmap.h
+++ OpenGL_Flightsim/clipmap.h
@@ -93,18 +93,64 @@ void create_plane(std::vector<glm::vec3>& vertices, std::vector<unsigned int>& i
 	}
 }
 
+
+struct Block {
+	gfx::VertexBuffer vbo;
+	gfx::ElementBufferObject ebo;
+	gfx::VertexArrayObject vao;
+	unsigned int index_count;
+
+	Block(int width, int height, float segment_size)
+	{
+		printf("w = %d, h = %d, s = %f\n", width, height, segment_size);
+		std::vector<glm::vec3> vertices;
+		std::vector<unsigned int> indices;
+
+		generate_mesh(vertices, indices, width, height, segment_size);
+
+		index_count = indices.size();
+		std::cout << "indices = " << indices.size() << std::endl;
+		std::cout << "vertices = " << vertices.size() / 3 << std::endl;
+
+		assert(indices.size() > 0 && vertices.size() > 0);
+
+		vao.bind();
+		vbo.buffer(&vertices[0], vertices.size() * sizeof(vertices[0]));
+		ebo.buffer(&indices[0], indices.size() * sizeof(indices[0]));
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+
+		vbo.unbind();
+		vao.unbind();
+
+
+	}
+
+	void bind()
+	{
+		vao.bind();
+	}
+
+	void draw()
+	{
+		bind();
+		glDrawElements(GL_TRIANGLE_STRIP, index_count, GL_UNSIGNED_INT, 0);
+	}
+
+};
+
+
 class Clipmap : public gfx::Object3D {
 public:
 	Clipmap() 
-		: shader("shaders/clipmap")
+		: shader("shaders/clipmap"),
+		segments(3),
+		segment_size(2.0f),
+		block(3, 3, 2.0f),
+		fix_up(2, 3, 2.0f)
 	{
-		// https://www.learnopengles.com/tag/vertex-buffer-object/
-		// https://www.learnopengles.com/tag/degenerate-triangles/
-		// https://www.learnopengles.com/android-lesson-eight-an-introduction-to-index-buffer-objects-ibos/
-
-		const float tile_height = 1.0f;
-		const float tile_width = 1.5f;
-
+#if 0
 		std::vector<glm::vec3> vertices;
 		std::vector<unsigned int> indices;
 
@@ -125,14 +171,16 @@ public:
 
 		tile_vbo.unbind();
 		tile_vao.unbind();
+#endif
 	}
 
-	glm::mat4 transform_matrix(const glm::vec2& position, float scale)
+	glm::mat4 transform_matrix(const glm::vec2& position, float scale, float angle = 0)
 	{
-		auto S = glm::scale(glm::mat4(1.0f), glm::vec3(scale));
-		auto T = glm::translate(glm::mat4(1.0f), glm::vec3(position.x, 0.0f, position.y));
-		return T * S;
-
+		const auto one = glm::mat4(1.0f);
+		auto S = glm::scale(one, glm::vec3(scale));
+		auto T = glm::translate(one, glm::vec3(position.x, 0.0f, position.y));
+		auto R = glm::rotate(one, angle, glm::vec3(0.0f, 1.0f, 0.0f));
+		return T * R * S;
 	}
 
 	void draw(gfx::RenderContext& context) override
@@ -144,16 +192,14 @@ public:
 			glm::vec2 camera_pos_xy = glm::vec2(camera_pos.x, camera_pos.z);
 
 			shader.bind();
-			shader.uniform("u_View",			context.camera->get_view_matrix());
-			shader.uniform("u_Projection",		context.camera->get_projection_matrix());
-
-			tile_vao.bind();
+			shader.uniform("u_View", context.camera->get_view_matrix());
+			shader.uniform("u_Projection", context.camera->get_projection_matrix());
 
 			bool wireframe = true;
+
 			//glFrontFace(GL_CW);
 			glEnable(GL_PRIMITIVE_RESTART);
 			glPrimitiveRestartIndex(primitive_restart);
-
 			if (wireframe) glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 			for (int l = 0; l <= levels; l++)
@@ -163,17 +209,15 @@ public:
 
 				float scale = pow(2.0f, l);
 				float next_scale = pow(2.0f, l+2);
-				float tile_size = segments * (segment_size * scale);
+				float scaled_segment_size = segment_size * scale;
+				float tile_size = segments * scaled_segment_size;
 				glm::vec2 offset = { tile_size + border, tile_size + border };
 
 
 				glm::vec2 snapped = glm::floor(camera_pos_xy / next_scale) * next_scale;
 				glm::vec2 start = snapped -  offset * 2.0f;
 
-
 				glm::vec3 tmp_offset(0.0f);
-
-
 
 				for (int r = 0; r < rows; r++)
 				{
@@ -186,60 +230,58 @@ public:
 						(
 							((r == 0 || r == rows - 1) || 
 							(c == 0 || c == cols - 1)) 
-							&& (c != 2) && (r != 2)
+							//&& (c != 2) && (r != 2)
 						)
 						{
 
-							
-							
-							//auto tile_pos = start + glm::vec2(r * offset.x, c * offset.y);
 							auto tile_pos = start + glm::vec2(tmp_offset.x, tmp_offset.y);
-
 							shader.uniform("u_Model", transform_matrix(tile_pos, scale));
 							shader.uniform("u_Level", static_cast<float>(l) / levels);
-							glDrawElements(GL_TRIANGLE_STRIP, index_count, GL_UNSIGNED_INT, 0);
+
+							if ((c != 2) && (r != 2))
+							{
+								block.draw();
+							}
+							else if(c == 2)
+							{
+								fix_up.draw();
+							}
+							else
+							{
+								//auto pos = tile_pos;
+								auto pos = tile_pos + glm::vec2(2 * scaled_segment_size, 0);
+								shader.uniform("u_Model", transform_matrix(pos, scale, -gfx::PI / 2));
+								fix_up.draw();
+							}
 						}
 
 #if 1
 						if (c == 2)
 						{
-							tmp_offset.y += 2 * segment_size * scale;
+							tmp_offset.y += 2 * scaled_segment_size;
 						}
 						else
 						{
 							tmp_offset.y += tile_size;
 						}
-
-
 #else
 						tmp_offset.x += tile_size;
 						tmp_offset.y += tile_size;
 #endif
-
 					}
 
 					if (r == 2)
 					{
-						tmp_offset.x += 2 * segment_size * scale;
+						tmp_offset.x += 2 * scaled_segment_size;
 					}
 					else
 					{
 						tmp_offset.x += tile_size;
 					}
 				}
-
 			}
 
-		
-
-
-
-
-
 			if (wireframe) glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-			tile_vao.unbind();
-
-
 			//glFrontFace(GL_CCW);
 		}
 	}
@@ -248,11 +290,16 @@ private:
 	gfx::Shader shader;
 	//gfx::Texture heightmap;
 
+#if 0
 	gfx::VertexBuffer tile_vbo;
 	gfx::ElementBufferObject tile_ebo;
 	gfx::VertexArrayObject tile_vao;
+#else
+	Block block; 
+	Block fix_up; 
+#endif
 
-	unsigned int index_count;
+	unsigned int index_count = 0;
 	const int levels = 3;
 	const int segments = 3;
 	const float segment_size = 2.0f;
