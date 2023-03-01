4725a2a1f6fabb0d0caf260598ea04c904b0a616
Jakob <jakob.g.maier@gmail.com>
2023-03-01 10:39:41 +0100

    moved and formatted with google style guide

---
+++ OpenGL_Flightsim/src/flightmodel.h
@@ -0,0 +1,227 @@
+#pragma once
+
+#include <algorithm>
+#include <cmath>
+#include <limits>
+#include <tuple>
+#include <vector>
+
+#include "data.h"
+#include "gfx.h"
+#include "phi.h"
+
+// get temperture in kelvin
+inline float get_air_temperature(float altitude) {
+  return 288.15f - 0.0065f * altitude;  // kelvin
+}
+
+// only accurate for altitudes < 11km
+float get_air_density(float altitude) {
+  assert(0.0f <= altitude && altitude <= 11000.0f);
+  float temperature = get_air_temperature(altitude);  // kelvin
+  float pressure =
+      101325.0f * std::pow(1 - 0.0065f * (altitude / 288.15f), 5.25f);
+  return 0.00348f * (pressure / temperature);
+}
+
+float get_propellor_thrust(const phi::RigidBody& rb, float engine_horsepower,
+                           float propellor_rpm, float propellor_diameter) {
+  float speed = rb.get_speed();
+  float engine_power = phi::units::watts(engine_horsepower);
+
+#if 1
+  const float a = 1.83f, b = -1.32f;
+  float propellor_advance_ratio =
+      speed / ((propellor_rpm / 60.0f) * propellor_diameter);
+  float propellor_efficiency =
+      a * propellor_advance_ratio + std::pow(b * propellor_advance_ratio, 3.0f);
+#else
+  float propellor_efficiency = 1.0f;
+#endif
+
+#if 1
+  const float C = 0.12f;
+  float air_density = get_air_density(rb.position.y);
+  float sea_level_air_density = get_air_density(0.0f);
+  float power_drop_off_factor =
+      ((air_density / sea_level_air_density) - C) / (1 - C);
+#else
+  float power_drop_off_factor = 1.0f;
+#endif
+
+  return ((propellor_efficiency * engine_power) / speed) *
+         power_drop_off_factor;
+}
+
+// https://aerotoolbox.com/airspeed-conversions/
+float get_indicated_air_speed(const phi::RigidBody& rb) {
+  const float airspeed = rb.get_speed();
+  const float air_density = get_air_density(rb.position.y);
+  const float sea_level_air_density = get_air_density(0.0f);
+  const float dynamic_pressure =
+      0.5f * air_density * phi::sq(airspeed);  // bernoulli's equation
+  return std::sqrt(2 * dynamic_pressure / sea_level_air_density);
+}
+
+// get g-force in pitch direction
+float get_g_force(const phi::RigidBody& rb) {
+  auto velocity = rb.get_body_velocity();
+  auto angular_velocity = rb.angular_velocity;
+
+  // avoid division by zero
+  float turn_radius = (std::abs(angular_velocity.z) < phi::EPSILON)
+                          ? std::numeric_limits<float>::max()
+                          : velocity.x / angular_velocity.z;
+
+  // centrifugal force = mass * velocity^2 / radius
+  // centrifugal acceleration = force / mass
+  // simplified, this results in:
+  float centrifugal_acceleration = phi::sq(velocity.x) / turn_radius;
+
+  float g_force = centrifugal_acceleration / phi::EARTH_GRAVITY;
+  g_force += (rb.up().y * phi::EARTH_GRAVITY) /
+             phi::EARTH_GRAVITY;  // add earth gravity
+  return g_force;
+}
+
+float get_mach_number(const phi::RigidBody& rb) {
+  float speed = rb.get_speed();
+  float temperature = get_air_temperature(rb.position.y);
+  float speed_of_sound = std::sqrt(1.402f * 286.f * temperature);
+  return speed / speed_of_sound;
+}
+
+struct Airfoil {
+  float min_alpha, max_alpha;
+  std::vector<glm::vec3> data;
+
+  Airfoil(const std::vector<glm::vec3>& curve_data) : data(curve_data) {
+    min_alpha = curve_data[0].x;
+    max_alpha = curve_data[curve_data.size() - 1].x;
+  }
+
+  std::tuple<float, float> sample(float alpha) const {
+    int max_index = static_cast<int>(data.size() - 1);
+    int index = static_cast<int>(phi::utils::scale(
+        alpha, min_alpha, max_alpha, 0.0f, static_cast<float>(max_index)));
+    assert(0 <= index && index < max_index);
+    return {data[index].y, data[index].z};
+  }
+};
+
+Airfoil NACA_0012(NACA_0012_data);
+Airfoil NACA_2412(NACA_2412_data);
+
+struct Engine : public phi::ForceEffector {
+  float throttle = 0.25f;
+  float thrust = 10000.0f;
+
+  Engine(float thrust) : thrust(thrust) {}
+
+  void apply_forces(phi::RigidBody& rigid_body) override {
+    float force = thrust * throttle;
+    rigid_body.add_relative_force({force, 0.0f, 0.0f});
+  }
+};
+
+struct Wing : public phi::ForceEffector {
+  const float area{};  // m^2
+  const Airfoil* airfoil;
+  const glm::vec3 normal;
+  const glm::vec3 center_of_pressure;
+
+  float lift_multiplier = 1.0f;
+  float drag_multiplier = 1.0f;
+  phi::Degrees deflection = 0.0f;
+
+  Wing(const glm::vec3& position, float area, const Airfoil* aero,
+       const glm::vec3& normal = phi::UP)
+      : center_of_pressure(position),
+        area(area),
+        airfoil(aero),
+        normal(normal) {}
+
+  Wing(const glm::vec3& position, float wingspan, float chord,
+       const Airfoil* aero, const glm::vec3& normal = phi::UP)
+      : center_of_pressure(position),
+        area(chord * wingspan),
+        airfoil(aero),
+        normal(normal) {}
+
+  void apply_forces(phi::RigidBody& rigid_body) override {
+    glm::vec3 local_velocity =
+        rigid_body.get_point_velocity(center_of_pressure);
+    float speed = glm::length(local_velocity);
+
+    if (speed <= 0.0f) return;
+
+    glm::vec3 wing_normal = normal;
+
+    if (std::abs(deflection) > phi::EPSILON) {
+      // rotate wing
+      auto axis = glm::normalize(glm::cross(phi::FORWARD, normal));
+      auto rotation =
+          glm::rotate(glm::mat4(1.0f), glm::radians(deflection), axis);
+      wing_normal = glm::vec3(rotation * glm::vec4(normal, 1.0f));
+    }
+
+    // drag acts in the opposite direction of velocity
+    glm::vec3 drag_direction = glm::normalize(-local_velocity);
+
+    // lift is always perpendicular to drag
+    glm::vec3 lift_direction = glm::normalize(
+        glm::cross(glm::cross(drag_direction, wing_normal), drag_direction));
+
+    // angle between chord line and air flow
+    float angle_of_attack =
+        glm::degrees(std::asin(glm::dot(drag_direction, wing_normal)));
+
+    // sample our aerodynamic data
+    auto [lift_coefficient, drag_coefficient] =
+        airfoil->sample(angle_of_attack);
+
+    // air density depends on altitude
+    float air_density = get_air_density(rigid_body.position.y);
+
+    float tmp = 0.5f * std::pow(speed, 2) * air_density * area;
+    glm::vec3 lift = lift_direction * lift_coefficient * lift_multiplier * tmp;
+    glm::vec3 drag = drag_direction * drag_coefficient * drag_multiplier * tmp;
+
+    // aerodynamic forces are applied at the center of pressure
+    rigid_body.add_force_at_point(lift + drag, center_of_pressure);
+  }
+};
+
+struct Aircraft {
+  Engine engine;
+  std::vector<Wing> elements;
+  phi::RigidBody rigid_body;
+  glm::vec3 joystick{};  // roll, yaw, pitch
+
+  Aircraft(float mass, float thrust, glm::mat3 inertia, std::vector<Wing> wings)
+      : elements(wings),
+        rigid_body({.mass = mass, .inertia = inertia}),
+        engine(thrust) {}
+
+  void update(phi::Seconds dt) {
+    float roll = joystick.x;
+    float yaw = joystick.y;
+    float pitch = joystick.z;
+    float max_elevator_deflection = 5.0f, max_aileron_deflection = 15.0f,
+          max_rudder_deflection = 3.0f;
+    float aileron_deflection = roll * max_aileron_deflection;
+
+    elements[1].deflection = +aileron_deflection;
+    elements[2].deflection = -aileron_deflection;
+    elements[4].deflection = -(pitch * max_elevator_deflection);
+    elements[5].deflection = yaw * max_rudder_deflection;
+
+    for (Wing& wing : elements) {
+      wing.apply_forces(rigid_body);
+    }
+
+    engine.apply_forces(rigid_body);
+
+    rigid_body.update(dt);
+  }
+};
---
+++ OpenGL_Flightsim/src/phi.h
@@ -0,0 +1,297 @@
+#pragma once
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/euler_angles.hpp>
+#include <glm/gtx/vector_angle.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/vec3.hpp>
+
+namespace phi {
+
+typedef float Seconds;
+typedef float Radians;
+typedef float Degrees;
+
+// constants
+constexpr float EARTH_GRAVITY = 9.80665f;  // gravity of earth, m/s^2
+constexpr float EPSILON = 1e-8f;
+
+// directions in body space
+constexpr glm::vec3 UP = {0.0f, 1.0f, 0.0f};
+constexpr glm::vec3 DOWN = {0.0f, -1.0f, 0.0f};
+constexpr glm::vec3 RIGHT = {0.0f, 0.0f, 1.0f};
+constexpr glm::vec3 LEFT = {0.0f, 0.0f, -1.0f};
+constexpr glm::vec3 FORWARD = {1.0f, 0.0f, 0.0f};
+constexpr glm::vec3 BACKWARD = {-1.0f, 0.0f, 0.0f};
+
+constexpr glm::vec3 X_AXIS = {1.0f, 0.0f, 0.0f};
+constexpr glm::vec3 Y_AXIS = {0.0f, 1.0f, 0.0f};
+constexpr glm::vec3 Z_AXIS = {0.0f, 0.0f, 1.0f};
+
+template <typename T>
+constexpr inline T sq(T a) {
+  return a * a;
+}
+
+template <typename T>
+constexpr inline T cb(T a) {
+  return a * a * a;
+}
+
+namespace inertia {
+struct Element {
+  float mass;
+  glm::vec3 position;  // position in design coordinates
+  glm::vec3 inertia;
+  glm::vec3 offset;  // offset from center of gravity
+};
+
+constexpr glm::vec3 cube(const glm::vec3& size, float mass) {
+  const float C = (1.0f / 12.0f) * mass;
+  glm::vec3 I(0.0f);
+  I.x = C * (sq(size.y) + sq(size.z));
+  I.y = C * (sq(size.x) + sq(size.z));
+  I.z = C * (sq(size.x) + sq(size.y));
+  return I;
+}
+
+constexpr glm::vec3 cylinder(float radius, float length, float mass) {
+  const float C = (1.0f / 12.0f) * mass;
+  glm::vec3 I(0.0f);
+  I.x = (0.5f) * mass * sq(radius);
+  I.y = I.z = C * (3.0f * sq(radius) + sq(length));
+  return I;
+}
+
+// inertia tensor
+constexpr glm::mat3 tensor(const glm::vec3& moment_of_inertia) {
+  return {
+      moment_of_inertia.x, 0.0f, 0.0f, 0.0f,
+      moment_of_inertia.y, 0.0f, 0.0f, 0.0f,
+      moment_of_inertia.z,
+  };
+}
+
+constexpr Element cube_element(const glm::vec3& position, const glm::vec3& size,
+                               float mass) {
+  return {.mass = mass,
+          .position = position,
+          .inertia = cube(size, mass),
+          .offset = position};
+}
+
+// calculate inertia tensor from list of connected masses
+constexpr glm::mat3 tensor(std::vector<Element>& elements,
+                           bool precomputed_offset = false) {
+  float Ixx = 0, Iyy = 0, Izz = 0;
+  float Ixy = 0, Ixz = 0, Iyz = 0;
+
+  float mass = 0;
+  glm::vec3 moment(0.0f);
+
+  for (const auto& element : elements) {
+    mass += element.mass;
+    moment += element.mass * element.position;
+  }
+
+  const glm::vec3 center_of_gravity = moment / mass;
+
+  for (auto& element : elements) {
+    glm::vec3 offset;
+
+    if (!precomputed_offset) {
+      element.offset = element.position - center_of_gravity;
+    }
+
+    offset = element.offset;
+
+    Ixx += element.inertia.x + element.mass * (sq(offset.y) + sq(offset.z));
+    Iyy += element.inertia.y + element.mass * (sq(offset.z) + sq(offset.x));
+    Izz += element.inertia.z + element.mass * (sq(offset.x) + sq(offset.y));
+    Ixy += element.mass * (offset.x * offset.y);
+    Ixz += element.mass * (offset.x * offset.z);
+    Iyz += element.mass * (offset.y * offset.z);
+  }
+
+  return {Ixx, -Ixy, -Ixz, -Ixy, Iyy, -Iyz, -Ixz, -Iyz, Izz};
+}
+};  // namespace inertia
+
+namespace utils {
+
+template <typename T>
+constexpr inline T scale(T input, T in_min, T in_max, T out_min, T out_max) {
+  assert(in_min <= input && input <= in_max);
+  return (input - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+//
+template <typename T>
+constexpr inline T lerp(T a, T b, float t) {
+  return a + t * (b - a);
+}
+
+//
+template <typename T>
+constexpr inline float inverse_lerp(T a, T b, T v) {
+  return (v - a) / (b - a);
+}
+};  // namespace utils
+
+namespace units {
+constexpr inline float knots(float meter_per_second) {
+  return meter_per_second * 1.94384f;
+}
+
+constexpr inline float meter_per_second(float kilometer_per_hour) {
+  return kilometer_per_hour / 3.6f;
+}
+
+constexpr inline float kilometer_per_hour(float meter_per_second) {
+  return meter_per_second * 3.6f;
+}
+
+constexpr inline float kelvin(float celsius) { return celsius - 273.15f; }
+
+constexpr inline float watts(float horsepower) { return horsepower * 745.7f; }
+};  // namespace units
+
+struct RigidBodyParams {
+  float mass = 1.0f;
+  glm::mat3 inertia{};
+  glm::vec3 position{};
+  glm::vec3 velocity{};
+  glm::vec3 angular_velocity{};
+  glm::quat orientation = glm::quat(glm::vec3(0.0f));
+  bool apply_gravity = true;
+};
+
+class RigidBody {
+ private:
+  glm::vec3 m_force{};   // force vector in world space
+  glm::vec3 m_torque{};  // torque vector in body space
+
+ public:
+  float mass;                    // rigidbody mass in kg
+  glm::vec3 position{};          // position in world space
+  glm::quat orientation{};       // orientation in world space
+  glm::vec3 velocity{};          // velocity in world space
+  glm::vec3 angular_velocity{};  // angular velocity in object space, x
+                                 // represents rotation around x axis
+  glm::mat3 inertia{}, inverse_inertia{};  // inertia tensor
+  bool apply_gravity = true;
+  bool active = true;
+
+  RigidBody()
+      : RigidBody({.mass = 1.0f,
+                   .inertia =
+                       inertia::tensor(inertia::cube(glm::vec3(1.0f), 1.0f))}) {
+  }
+
+  RigidBody(const RigidBodyParams& params)
+      : mass(params.mass),
+        position(params.position),
+        velocity(params.velocity),
+        inertia(params.inertia),
+        orientation(params.orientation),
+        apply_gravity(params.apply_gravity),
+        angular_velocity(params.angular_velocity),
+        inverse_inertia(glm::inverse(params.inertia)) {}
+
+  // get velocity of point in body space
+  inline glm::vec3 get_point_velocity(const glm::vec3& point) const {
+    return inverse_transform_direction(velocity) +
+           glm::cross(angular_velocity, point);
+  }
+
+  // get velocity in body space
+  inline glm::vec3 get_body_velocity() const {
+    return inverse_transform_direction(velocity);
+  }
+
+  // force and point vectors are in body space
+  inline void add_force_at_point(const glm::vec3& force,
+                                 const glm::vec3& point) {
+    m_force += transform_direction(force);
+    m_torque += glm::cross(point, force);
+  }
+
+  // transform direction from body space to world space
+  inline glm::vec3 transform_direction(const glm::vec3& direction) const {
+    return orientation * direction;
+  }
+
+  // transform direction from world space to body space
+  inline glm::vec3 inverse_transform_direction(
+      const glm::vec3& direction) const {
+    return glm::inverse(orientation) * direction;
+  }
+
+  // set inertia tensor
+  inline void set_inertia(const glm::mat3& tensor) {
+    inertia = tensor, inverse_inertia = glm::inverse(tensor);
+  }
+
+  // force vector in world space
+  inline void add_force(const glm::vec3& force) { m_force += force; }
+
+  // force vector in body space
+  inline void add_relative_force(const glm::vec3& force) {
+    m_force += orientation * force;
+  }
+
+  // torque vector in world space
+  inline void add_torque(const glm::vec3& torque) {
+    m_torque += inverse_transform_direction(torque);
+  }
+
+  // torque vector in body space
+  inline void add_relative_torque(const glm::vec3& torque) {
+    m_torque += torque;
+  }
+
+  // get speed
+  inline float get_speed() const { return glm::length(velocity); }
+
+  // get torque in body space
+  inline glm::vec3 get_torque() const { return m_torque; }
+
+  // get torque in world space
+  inline glm::vec3 get_force() const { return m_force; }
+
+  // get forward direction in world space
+  inline glm::vec3 forward() const { return transform_direction(phi::FORWARD); }
+
+  // get up direction in world space
+  inline glm::vec3 up() const { return transform_direction(phi::UP); }
+
+  // get right direction in world space
+  inline glm::vec3 right() const { return transform_direction(phi::RIGHT); }
+
+  void update(Seconds dt) {
+    if (!active) return;
+
+    glm::vec3 acceleration = m_force / mass;
+
+    if (apply_gravity) acceleration.y -= EARTH_GRAVITY;
+
+    velocity += acceleration * dt;
+    position += velocity * dt;
+
+    angular_velocity +=
+        inverse_inertia *
+        (m_torque - glm::cross(angular_velocity, inertia * angular_velocity)) *
+        dt;
+    orientation +=
+        (orientation * glm::quat(0.0f, angular_velocity)) * (0.5f * dt);
+    orientation = glm::normalize(orientation);
+
+    // reset accumulators
+    m_force = glm::vec3(0.0f), m_torque = glm::vec3(0.0f);
+  }
+};
+
+struct ForceEffector {
+  virtual void apply_forces(phi::RigidBody& rigid_body) = 0;
+};
+};  // namespace phi
