5e594e72553a23df0888e418895aa9dbba5009d8
Jakob <jakob.g.maier@gmail.com>
2023-04-02 19:44:32 +0200

    update release

--- tools/release.bat
+++ tools/release.bat
@@ -3,9 +3,9 @@
 
 set ReleaseDir=x64\Release\
 
-set LIBS=C:\Users\jakob\Documents\libraries
-set SDL2=%LIBS%\SDL2\lib\x64\SDL2.dll
-set GLEW=%LIBS%\glew-2.1.0\bin\Release\x64\glew32.dll
+set LIBS=lib
+set SDL2=%LIBS%\SDL2-2.26.4\lib\x64\SDL2.dll
+set GLEW=%LIBS%\glew-2.2.0\bin\Release\x64\glew32.dll
 
 :: copy DLLs
 copy %SDL2% %ReleaseDir%
