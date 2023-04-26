9c249de76c0b885f89fc0d969ad6bd8411a88597
Jakob <jakob.g.maier@gmail.com>
2023-04-26 11:08:38 +0200

    update to release script

--- tools/release.bat
+++ tools/release.bat
@@ -1,5 +1,6 @@
 :: windows release script
 @echo off
+setlocal
 
 set ReleaseDir=x64\Release\
 
@@ -7,17 +8,27 @@ set LIBS=lib
 set SDL2=%LIBS%\SDL2-2.26.4\lib\x64\SDL2.dll
 set GLEW=%LIBS%\glew-2.2.0\bin\Release\x64\glew32.dll
 
+for /f "delims=" %%i in ('git rev-parse --abbrev-ref HEAD') do set Branch=%%i
+for /f "delims=" %%i in ('git rev-parse --short HEAD') do set Hash=%%i
+
+set Release=flightsim-win32-x64.zip
+
 :: copy DLLs
 copy %SDL2% %ReleaseDir%
 copy %GLEW% %ReleaseDir% 
 
 :: write version info to file
 (
+  echo Release created: 
   echo %DATE% %TIME%
-  git rev-parse HEAD
-) > %ReleaseDir%/VERSION
+  echo Git Hash:
+  git log -1 --pretty=oneline
+) > %ReleaseDir%/VERSION.txt
 
-del *.zip
+del %Release%
 
 :: compress 
-tar.exe --exclude *.pdb -acvf flightsim-win32-x64.zip -C %ReleaseDir% *
+tar.exe --exclude *.pdb -acvf %Release% -C %ReleaseDir% *
+
+echo Current branch: %Branch%
+echo Latest commit: %Hash%
