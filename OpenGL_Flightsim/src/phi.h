1728e58f94755bc39a7cc42fd78221545eed0a74
Jakob <45669207+gue-ni@users.noreply.github.com>
2023-06-05 15:13:05 +0200

    Update phi.h

--- OpenGL_Flightsim/src/phi.h
+++ OpenGL_Flightsim/src/phi.h
@@ -102,11 +102,7 @@ struct Transform {
   constexpr Transform(const glm::vec3& p, const glm::quat& o) : position(p), orientation(o) {}
   constexpr Transform() : position(glm::vec3(0.0f)), orientation(glm::quat(1.0f, 0.0f, 0.0f, 0.0f)) {}
   
-  std::vector<glm::vec3> axes() const
-  {
-    auto axes = glm::mat3(matrix());
-    return {axes[0], axes[1], axes[2]};
-  }
+  
   
   glm::mat4 matrix() const { return glm::translate(glm::mat4(1.0f), position) * glm::mat4(orientation); }
 };
@@ -120,7 +116,17 @@ namespace hitbox
 struct OBB : public Transform {
   glm::vec3 size;
   constexpr OBB(const glm::vec3& s) : Transform(), size(s) {}
-
+  
+  std::vector<glm::vec3> axes() const
+  {
+    auto axes = glm::mat3(matrix());
+    return {axes[0], axes[1], axes[2]};
+  }
+  
+  std::vector<glm::vec3> vertices() const {
+    
+  }
+  
   static constexpr glm::vec3 inertia(const glm::vec3& size, float mass)
   {
     const float C = (1.0f / 12.0f) * mass;
