ba7b5ee0f9c9f529c3048b130f27e358b22607a9
Jakob <jakob.g.maier@gmail.com>
2023-08-15 12:40:31 +0200

    implemented actual shadowmap

--- OpenGL_Flightsim/src/gfx/gfx.cpp
+++ OpenGL_Flightsim/src/gfx/gfx.cpp
@@ -388,6 +388,8 @@ ShadowMap::ShadowMap(int w, int h) : width(w), height(h)
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+  float borderColor[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+  glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);  
   texture->unbind();
 
   framebuffer.bind();
