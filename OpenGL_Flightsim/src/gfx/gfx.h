6f15f371184b450799516a19b59d9605d1a9a2f3
Jakob <45669207+gue-ni@users.noreply.github.com>
2023-09-08 09:15:51 +0200

    Update gfx.h

--- OpenGL_Flightsim/src/gfx/gfx.h
+++ OpenGL_Flightsim/src/gfx/gfx.h
@@ -210,6 +210,7 @@ public:
   void batch_line(const Line& line, float angle);
   void batch_line(const Line& line, const glm::mat4& matrix);
   void batch_line(float width, const glm::mat4& matrix);
+  void batch_circle(const glm::vec3& center, float radius);
   void batch_clear();
 private:
   std::vector<Line> m_lines;
